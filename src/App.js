5794e4c8379d117d6f8259d0ec4dfcce8060887c
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-11-02 18:24:32 -0800

    creating useFetchJobs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import useFetchJobs from "./hooks/useFetchJobs";
+
 function App() {
-  return <div>Hello World</div>;
+  const { jobs, loading, error } = useFetchJobs();
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+  );
 }
 
 export default App;
