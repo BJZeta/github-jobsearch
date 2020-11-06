8e34ba3c5af63c8e13de426af0ae3e81c6f09f2b
Brandon Unzueta <unzueta.brandon@gmail.com>
2020-11-05 19:44:19 -0800

    creating searchform component/fixing action has_update

--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -66,7 +66,7 @@ export default function useFetchJobs(params, page) {
       .then((res) => {
         dispatch({
           type: ACTIONS.UPDATE_HAS_NEXT_PAGE,
-          payload: { hasNextPage: res.data.legnth !== 0 },
+          payload: { hasNextPage: res.data.length !== 0 },
         });
       })
       .catch((err) => {
